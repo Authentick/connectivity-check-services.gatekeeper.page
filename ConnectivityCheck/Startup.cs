172be9f8b06a702248bd860de2fc556e4e8eed16
Lukas Reschke <lukas@statuscode.ch>
2020-12-23 15:55:07 +0000

    Remove unnecessary imports

--- ConnectivityCheck/Startup.cs
+++ ConnectivityCheck/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace ConnectivityCheck
@@ -48,8 +41,6 @@ namespace ConnectivityCheck
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
