f4cf92470edb3bc0daa78fb5e51e7692c99183bc
Lukas Reschke <lukas@statuscode.ch>
2020-12-23 18:44:19 +0000

    Prettify

--- index.js
+++ index.js
@@ -5,13 +5,13 @@
  */
 async function gatherResponse(response, challenge) {
   const { headers } = response
-  const contentType = headers.get("content-type") || ""
-  if (contentType.includes("application/json")) {
+  const contentType = headers.get('content-type') || ''
+  if (contentType.includes('application/json')) {
     const json = JSON.stringify(await response.json())
-    return json["challenge"] == challenge
+    return json['Challenge'] == challenge
   }
 
-  return false;
+  return false
 }
 
 /**
@@ -20,30 +20,31 @@ async function gatherResponse(response, challenge) {
 async function handleRequest(request) {
   const init = {
     headers: {
-      "content-type": "application/json;charset=UTF-8",
+      'content-type': 'application/json;charset=UTF-8',
     },
   }
 
   const body = await request.text()
   const params = JSON.parse(body)
-  const hostname = params["hostname"].trim()
-  const challenge = params["challenge"].trim()
+  const hostname = params['hostname'].trim()
+  const challenge = params['challenge'].trim()
 
-  const url = new URL("https://example.com/");
-  url.protocol = "http"
+  const url = new URL('http://example.com/')
+  url.protocol = 'http'
   url.hostname = hostname
-  url.pathname = "/api/connectivity_check"
+  url.pathname = '/api/connectivity_check'
+  url.searchParams.append("challenge", challenge)
 
   const response = await fetch(url, init)
   const successful = await gatherResponse(response, challenge)
 
   if (successful) {
-    return new Response(JSON.stringify({ "success": true }), init)
+    return new Response(JSON.stringify({ success: true }), init)
   }
 
-  return new Response(JSON.stringify({ "success": false }), init)
+  return new Response(JSON.stringify({ success: false }), init)
 }
 
-addEventListener("fetch", event => {
+addEventListener('fetch', event => {
   return event.respondWith(handleRequest(event.request))
 })
